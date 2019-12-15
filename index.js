1b9b8f764d88fdcceef84ba37570b4f7bbe5abef
Vinoth <vinoth186@gmail.com>
2019-12-15 23:37:58 +0530

    js

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser'); 
+const app = express();
+const port = 8080; 
+
+require('dotenv').config();
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.listen(process.env.PORT || port, () =>  {
+	console.log(`App listening on port ${port}!`)
+});
+
+app.get('/', (req, res) => {
+	console.log(req);    
+    res.send('Welcome')
+});
+
+app.post('/show', (req, res) => {
+	console.log(req);    
+    res.send('Welcome')
+});
