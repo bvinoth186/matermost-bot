c70a9614cdc6ae8f1282ca2a49142df972178586
Vinoth <vinoth186@gmail.com>
2019-12-16 00:14:02 +0530

    logic for matermost interactive dialog

--- index.js
+++ index.js
@@ -14,11 +14,150 @@ app.listen(process.env.PORT || port, () =>  {
 });
 
 app.get('/', (req, res) => {
-	console.log(req.query);    
+	console.log(req);    
     res.send('Welcome')
 });
 
 app.post('/show', (req, res) => {
-	console.log(req.body);    
-    res.send('Welcome')
+	console.log(req.body);  
+    let trigger_id = req.body.trigger_id;
+	
+	request.post({
+		'headers': { 'content-type': 'application/json' },
+		'url' : 'http://adea2b8a.ngrok.io/api/v4/actions/dialogs/open', 
+		'body': JSON.stringify({
+			'trigger_id':trigger_id,
+			   'url':'http://localhost:5000/dialog_submit',
+			   'dialog':{
+				  'callback_id':'somecallbackid',
+				  'title':'Test Title',
+				  'icon_url':'http://www.mattermost.org/wp-content/uploads/2016/04/icon.png',
+				  'elements':[
+					 {
+						'display_name':'Display Name',
+						'name':'realname',
+						'type':'text',
+						'subtype':'',
+						'default':'default text',
+						'placeholder':'placeholder',
+						'help_text':'This a regular input in an interactive dialog triggered by a test integration.',
+						'optional':false,
+						'min_length':0,
+						'max_length':0,
+						'data_source':'',
+						'options':null
+					 },
+					 {
+						'display_name':'Email',
+						'name':'someemail',
+						'type':'text',
+						'subtype':'email',
+						'default':'',
+						'placeholder':'placeholder@bladekick.com',
+						'help_text':'This a regular email input in an interactive dialog triggered by a test integration.',
+						'optional':false,
+						'min_length':0,
+						'max_length':0,
+						'data_source':'',
+						'options':null
+					 },
+					 {
+						'display_name':'Number',
+						'name':'somenumber',
+						'type':'text',
+						'subtype':'number',
+						'default':'',
+						'placeholder':'',
+						'help_text':'',
+						'optional':false,
+						'min_length':0,
+						'max_length':0,
+						'data_source':'',
+						'options':null
+					 },
+					 {
+						'display_name':'Display Name Long Text Area',
+						'name':'realnametextarea',
+						'type':'textarea',
+						'subtype':'',
+						'default':'',
+						'placeholder':'placeholder',
+						'help_text':'',
+						'optional':true,
+						'min_length':5,
+						'max_length':100,
+						'data_source':'',
+						'options':null
+					 },
+					 {
+						'display_name':'User Selector',
+						'name':'someuserselector',
+						'type':'select',
+						'subtype':'',
+						'default':'',
+						'placeholder':'Select a user...',
+						'help_text':'',
+						'optional':false,
+						'min_length':0,
+						'max_length':0,
+						'data_source':'users',
+						'options':null
+					 },
+					 {
+						'display_name':'Channel Selector',
+						'name':'somechannelselector',
+						'type':'select',
+						'subtype':'',
+						'default':'',
+						'placeholder':'Select a channel...',
+						'help_text':'Choose a channel from the list.',
+						'optional':true,
+						'min_length':0,
+						'max_length':0,
+						'data_source':'channels',
+						'options':null
+					 },
+					 {
+						'display_name':'Option Selector',
+						'name':'someoptionselector',
+						'type':'select',
+						'subtype':'',
+						'default':'',
+						'placeholder':'Select an option...',
+						'help_text':'',
+						'optional':false,
+						'min_length':0,
+						'max_length':0,
+						'data_source':'',
+						'options':[
+						   {
+							  'text':'Option1',
+							  'value':'opt1'
+						   },
+						   {
+							  'text':'Option2',
+							  'value':'opt2'
+						   },
+						   {
+							  'text':'Option3',
+							  'value':'opt3'
+						   }
+						]
+					 }
+				  ],
+				  'submit_label':'Submit',
+				  'notify_on_cancel':true,
+				  'state':'somestate'
+			   }
+		})
+	},(error, response, body) => {
+		if(error) {
+			res.send(error)
+		}
+		var jsonContent = JSON.parse(body);
+		
+		console.log(jsonContent);
+		res.send('Done')
+	});
+    
 });
